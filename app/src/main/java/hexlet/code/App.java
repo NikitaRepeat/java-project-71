3d032a270ee08c5ad43831b151aaa7dce4795c7e
Piskun Nikita <n.piskun2014@yandex.ru>
2023-05-30 14:00:06 +0300

    add picocli implementation in build and make output with picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(
+        name = "gendiff",
+        version = "2.0",
+        mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+
+public final class App implements Callable {
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        return null;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        new CommandLine(new App()).execute(args);
     }
 }
